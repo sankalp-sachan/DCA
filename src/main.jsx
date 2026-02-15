06072a97e5b415a7a960b24d071ae7c2e7c1fa37
Sankalp Sachan <sankalpsachan2007@gmail.com>
2026-02-15 12:31:05 +0530

    Fix residual PWA 404s: unregister service workers and add favicon.ico

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Unregister any existing service workers
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+            registration.unregister();
+        }
+    });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
